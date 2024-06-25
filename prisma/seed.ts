38699b945aed827d1bacd329cf58d1ba6099a65d
LiuLiu <liuliu.developer@gmail.com>
2024-06-25 11:39:41 -0700

    format

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,78 +1,80 @@
-import { PrismaClient } from '@prisma/client';
-import csv from 'csv-parser';
-import fs from 'fs';
+import { PrismaClient } from "@prisma/client";
+import csv from "csv-parser";
+import fs from "fs";
 
 const prisma = new PrismaClient();
 
-async function processCSV(filePath: string, processData: (data: any[]) => Promise<void>): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        const results: any[] = [];
-        fs.createReadStream(filePath)
-            .pipe(csv())
-            .on('data', (data) => results.push(data))
-            .on('end', async () => {
-                try {
-                    await processData(results);
-                    resolve();
-                } catch (error) {
-                    reject(error);
-                }
-            });
-    });
+async function processCSV(
+  filePath: string,
+  processData: (data: any[]) => Promise<void>
+): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    const results: any[] = [];
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", (data) => results.push(data))
+      .on("end", async () => {
+        try {
+          await processData(results);
+          resolve();
+        } catch (error) {
+          reject(error);
+        }
+      });
+  });
 }
 
 async function main() {
-    try {
-        await processCSV('./employees.csv', async (data) => {
-            for (const emp of data) {
-                await prisma.employee.create({
-                    data: {
-                        emp_no: parseInt(emp.emp_no),
-                        last_name: emp.last_name,
-                        first_name: emp.first_name,
-                        birth_date: new Date(emp.birth_date),
-                        hire_date: new Date(emp.hire_date),
-                        gender: emp.gender,
-                    }
-                });
-            }
-            console.log('Employees have been inserted');
+  try {
+    await processCSV("./employees.csv", async (data) => {
+      for (const emp of data) {
+        await prisma.employee.create({
+          data: {
+            emp_no: parseInt(emp.emp_no),
+            last_name: emp.last_name,
+            first_name: emp.first_name,
+            birth_date: new Date(emp.birth_date),
+            hire_date: new Date(emp.hire_date),
+            gender: emp.gender,
+          },
         });
+      }
+      console.log("Employees have been inserted");
+    });
 
-        await processCSV('./departments.csv', async (data) => {
-            for (const dept of data) {
-                await prisma.department.create({
-                    data: {
-                        dept_no: dept.dept_no,
-                        dept_name: dept.dept_name,
-                    }
-                });
-            }
-            console.log('Departments have been inserted');
+    await processCSV("./departments.csv", async (data) => {
+      for (const dept of data) {
+        await prisma.department.create({
+          data: {
+            dept_no: dept.dept_no,
+            dept_name: dept.dept_name,
+          },
         });
+      }
+      console.log("Departments have been inserted");
+    });
 
-        await processCSV('./dept_emp.csv', async (data) => {
-            for (const deptEmp of data) {
-                await prisma.deptEmp.create({
-                    data: {
-                        emp_no: parseInt(deptEmp.emp_no),
-                        dept_no: deptEmp.dept_no,
-                        from_date: new Date(deptEmp.from_date),
-                        to_date: new Date(deptEmp.to_date),
-                    }
-                });
-            }
-            console.log('Department_Employee have been inserted');
+    await processCSV("./dept_emp.csv", async (data) => {
+      for (const deptEmp of data) {
+        await prisma.deptEmp.create({
+          data: {
+            emp_no: parseInt(deptEmp.emp_no),
+            dept_no: deptEmp.dept_no,
+            from_date: new Date(deptEmp.from_date),
+            to_date: new Date(deptEmp.to_date),
+          },
         });
-
-    } catch (e) {
-        console.error('Error during database operation', e);
-    } finally {
-        await prisma.$disconnect();
-    }
+      }
+      console.log("Department_Employee have been inserted");
+    });
+  } catch (e) {
+    console.error("Error during database operation", e);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-main().catch(e => {
-    console.error(e);
-    process.exit(1);
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,84 @@
-import {  PrismaClient } from '@prisma/client'
-import { doltCommit,printCommitLog,printStatus, createBranch ,printActiveBranch,checkoutBranch, printDiff } from "./doltUtils";
-import { PrismaTransaction } from './types';
+import { PrismaClient } from "@prisma/client";
+import {
+  doltCommit,
+  printCommitLog,
+  printStatus,
+  createBranch,
+  printActiveBranch,
+  checkoutBranch,
+  printDiff,
+} from "./doltUtils";
+import { PrismaTransaction } from "./types";
 
-
-async function createTablesCommit(){
-    const prisma = new PrismaClient()
-    try {
-        await doltCommit(prisma,"LiuLiu <liu@dolthub.com>", "Create tables");
-        await printCommitLog(prisma);
-        await printStatus(prisma);
-
-     } 
-    catch (error) {
-        console.error( error);
-    } finally {
-        await prisma.$disconnect();
-    }
+async function createTablesCommit() {
+  const prisma = new PrismaClient();
+  try {
+    await doltCommit(prisma, "LiuLiu <liu@dolthub.com>", "Create tables");
+    await printCommitLog(prisma);
+    await printStatus(prisma);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-createTablesCommit()
+createTablesCommit();
 
-
-async function updateManager(){
-    const prisma = new PrismaClient()
-    try {
-        await prisma.$transaction(async (tx) => {
-            await createBranch(tx, "add-manager");
-            await checkoutBranch(tx, "add-manager");
-            await printActiveBranch(tx);
-            await updateDepartmentManagers(tx);
-            await doltCommit(tx,"LiuLiu <liu@dolthub.com>", "Add managers");
-            await printCommitLog(tx);
-
-        });
-      
-        console.log("All operations completed successfully.");
-    } catch (error) {
-        console.error("An error occurred:", error);
-    } finally {
-        await prisma.$disconnect();
-    }
+async function updateManager() {
+  const prisma = new PrismaClient();
+  try {
+    await prisma.$transaction(async (tx) => {
+      await createBranch(tx, "add-manager");
+      await checkoutBranch(tx, "add-manager");
+      await printActiveBranch(tx);
+      await updateDepartmentManagers(tx);
+      await doltCommit(tx, "LiuLiu <liu@dolthub.com>", "Add managers");
+      await printCommitLog(tx);
+    });
+    console.log("All operations completed successfully.");
+  } catch (error) {
+    console.error("An error occurred:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-updateManager()
-
+updateManager();
 
 async function updateDepartmentManagers(prisma: PrismaTransaction) {
-     // Update Marketing
-    await prisma.department.update({
-      where: { dept_no: 'd001' },
-      data: { manager_id: 10001 },
-    });
+  // Update Marketing
+  await prisma.department.update({
+    where: { dept_no: "d001" },
+    data: { manager_id: 10001 },
+  });
 
-    // Update Finance
-    await prisma.department.update({
-      where: { dept_no: 'd002' },
-      data: { manager_id: 10002 },
-    });
+  // Update Finance
+  await prisma.department.update({
+    where: { dept_no: "d002" },
+    data: { manager_id: 10002 },
+  });
 
-    // Update Human Resources
-    await prisma.department.update({
-      where: { dept_no: 'd003' },
-      data: { manager_id: 10003 },
-    });
+  // Update Human Resources
+  await prisma.department.update({
+    where: { dept_no: "d003" },
+    data: { manager_id: 10003 },
+  });
 
-    // Update Production
-    await prisma.department.update({
-      where: { dept_no: 'd004' },
-      data: { manager_id: 10004 },
-    });
+  // Update Production
+  await prisma.department.update({
+    where: { dept_no: "d004" },
+    data: { manager_id: 10004 },
+  });
 
-    // Update Development
-    await prisma.department.update({
-      where: { dept_no: 'd005' },
-      data: { manager_id: 10005 },
-    });
+  // Update Development
+  await prisma.department.update({
+    where: { dept_no: "d005" },
+    data: { manager_id: 10005 },
+  });
 
-    console.log('Department managers updated successfully.');
- }
- const prisma = new PrismaClient()
+  console.log("Department managers updated successfully.");
+}
+const prisma = new PrismaClient();
 
- printDiff(prisma,"departments")
+printDiff(prisma, "departments");
